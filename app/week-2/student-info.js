5af15b191b5d02de71e8089abfe4ed1291de6bda
Ethan Go <egohobbies@gmail.com>
2025-01-28 21:12:45 -0700

    Added Student Info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return(
+        <div>
+            {/* <h1>Student Info</h1> */}
+            <p>John Ethan Go</p>
+            <p>Github repo: <Link href="https://github.com/john-ethan-go/cprg306-assignments">CPRG-306 Assignments</Link></p>
+        </div>
+
+
+
+
+    );
+}
