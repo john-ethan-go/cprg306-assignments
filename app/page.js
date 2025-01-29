5e26f16d5b3f5dd1962068879a410b493fa1cf85
Ethan Go <egohobbies@gmail.com>
2025-01-28 21:46:06 -0700

    Added root directory week 2

---
+++ app/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link"
+
+export default function Page() {
+return (
+  <div>
+    <h1> CPRG 306: Web Development 2 - Assignments</h1>
+    <p><Link href="week-2">Week 2 Assignment</Link></p>
+  </div>
+  );
+}
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -7,9 +7,5 @@ export default function StudentInfo() {
             <p>John Ethan Go</p>
             <p>Github repo: <Link href="https://github.com/john-ethan-go/cprg306-assignments">CPRG-306 Assignments</Link></p>
         </div>
-
-
-
-
     );
 }
